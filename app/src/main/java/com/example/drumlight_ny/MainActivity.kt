b85eb55a2bb9c0584f832f6b0c287bed8b9cd98d
ImWalpurgisnacht <kny010901@naver.com>
2023-08-04 11:11:26 +0900

    레이아웃 전체적으로 수정

--- app/src/main/java/com/example/drumlight_ny/MainActivity.kt
+++ app/src/main/java/com/example/drumlight_ny/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.drumlight_ny
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_popup)
+
+        val uiOptions = window.decorView.systemUiVisibility
+        var newUiOptions: Int = uiOptions
+        val isImmersiveModeEnabled = uiOptions or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY == uiOptions
+        if (isImmersiveModeEnabled) {
+            Log.i("Is on?", "Turning immersive mode mode off. ")
+        } else {
+            Log.i("Is on?", "Turning immersive mode mode on.")
+        }
+// 몰입 모드를 꼭 적용해야 한다면 아래의 3가지 속성을 모두 적용시켜야 합니다
+// 몰입 모드를 꼭 적용해야 한다면 아래의 3가지 속성을 모두 적용시켜야 합니다
+        newUiOptions = newUiOptions xor View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        newUiOptions = newUiOptions xor View.SYSTEM_UI_FLAG_FULLSCREEN
+        newUiOptions = newUiOptions xor View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        window.decorView.systemUiVisibility = newUiOptions
     }
+
 }
